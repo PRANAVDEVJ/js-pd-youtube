08f6ab27d9dedecc1410762032323fcf8b275aa0
PRANAVDEVJ <158818390+PRANAVDEVJ@users.noreply.github.com>
2024-07-18 16:24:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treates all js codes as newer version
+
+// alert (3+3) // we are using node js not browser
+
+console.log(3 + // code readability should be high
+    3 )
+
+console.log("pranav")
+
+let name = "pranav"
+let age = 18
+let isLoggedin = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
